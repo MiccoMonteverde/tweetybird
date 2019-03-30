1fba71b6f77ce83a7592bef8aad1132e59ae4a3f
Micco Monteverde <minenotnoob@gmail.com>
2019-03-30 20:13:43 +0800

    Creates full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
     content_for :title, title
     tag.h1 title
   end
+
+  def full_title(app_name)
+    tag.title(content_for?(:title) ? "#{content_for :title} | #{app_name}" : app_name)
+  end
 end
