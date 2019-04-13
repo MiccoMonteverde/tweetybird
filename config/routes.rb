b10f332cbfc28afaa3d445081b89537d5c12c569
Micco Monteverde <minenotnoob@gmail.com>
2019-04-13 21:33:54 +0800

    Adds site layout integration test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get "static_pages/home", to: "static_pages#home"
+  get "static_pages/help", to: "static_pages#help"
+  get "static_pages/about", to: "static_pages#about"
+  get "static_pages/contact", to: "static_pages#contact"
   get "/", to: redirect("static_pages/home") # redirects root to home page
   root "static_pages#home"
 
