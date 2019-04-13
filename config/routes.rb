69aa16f33bef0cf4b27db983049e740d385f6379
Micco Monteverde <minenotnoob@gmail.com>
2019-04-13 21:37:36 +0800

    Generates users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get "static_pages/home", to: "static_pages#home"
   get "static_pages/help", to: "static_pages#help"
   get "static_pages/about", to: "static_pages#about"
