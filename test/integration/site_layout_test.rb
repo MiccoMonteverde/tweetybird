b6587e103ca45ab2a189f2a6a546a1c70e10fed0
Micco Monteverde <minenotnoob@gmail.com>
2019-04-14 18:49:31 +0800

    Add signup to integration test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,6 +5,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get static_pages_home_path
     assert_template "static_pages/home"
     assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", signup_path, count: 2
     assert_select "a[href=?]", static_pages_help_path
     assert_select "a[href=?]", static_pages_about_path
     assert_select "a[href=?]", static_pages_contact_path
